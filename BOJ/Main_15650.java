28668d5e8f831000ce0f6d70ba878a9175231ce0
lapera00 <86655589+lapera00@users.noreply.github.com>
2023-08-11 16:59:57 +0900

    Add files via upload

---
+++ BOJ/Main_15650.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main_15650 {
+		
+	static int N, M;
+	static int[] arr;
+	static StringBuilder sb = new StringBuilder();
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		
+		N = Integer.parseInt(st.nextToken());  //몇까지
+		M = Integer.parseInt(st.nextToken());  //수열 크기
+		
+		arr = new int[M];
+
+		sol(1, 0);
+		
+		System.out.println(sb);
+		
+		
+	}
+	
+	static void sol(int s, int dep) {
+		if(dep == M) {
+			for(int i = 0; i< M; i++) {
+				sb.append(arr[i]).append(" ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = s; i <= N; i++){
+			arr[dep] = i;
+			sol(i+1, dep+1);
+			
+		}
+		
+	}
+
+}
